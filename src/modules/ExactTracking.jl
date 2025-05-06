c0361cc9a73ae5a51c6a7103e85145a087d9efd6
dtabell <dtabell@users.noreply.github.com>
2025-05-06 13:23:05 -0600

    Finish DKD kernel for multipole.

--- src/modules/ExactTracking.jl
+++ src/modules/ExactTracking.jl
@@ -49,8 +49,15 @@ In the computation of z_final, we use the fact that
 to avoid the potential for severe cancellation when
 a and b both have the form 1 + ε for different small
 values of ε.
+
+Arguments
+—————————
+beta_0:   β_0 = (βγ)_0 / √(γ_0^2)
+gamsqr_0: γ_0^2 = 1 + (βγ)_0^2
+tilde_m:  1 / (βγ)_0  # mc^2 / p0·c
+L: element length
 """
-@inline function exact_drift!(i, v, work, tilde_m, gamsqr_0, beta_0, L)
+@inline function exact_drift!(i, v, work, beta_0, gamsqr_0, tilde_m, L)
   @assert size(work, 2) >= 1 && size(work, 1) == N_particle "Size of work matrix must be at least ($N_particle, 1) for exact_drift!()."
   @inbounds begin @FastGTPSA! begin
     work[i,1] = sqrt((1.0 + v[i,PZI])^2 - (v[i,PXI]^2 + v[i,PYI]^2))  # P_s
@@ -75,20 +82,24 @@ end # function exact_drift!()
 """
 mkm_quadrupole!()
 
-This integrator uses the so-called Matrix-Kick-Matrix method to implement
-a quadrupole integrator accurate though second-order in the integration
-step-size.
+This integrator uses Matrix-Kick-Matrix to implement a quadrupole
+integrator accurate though second-order in the integration step-size.
 
-beta_gamma_0: reference value of βγ
-k2_num: g / Bρ0 = g / (p0 / q)
-        where g and Bρ0 respectively denote the quadrupole gradient and
-        (signed) reference magnetic rigidity.
+Arguments
+—————————
+beta_0:   β_0 = (βγ)_0 / √(γ_0^2)
+gamsqr_0: γ_0^2 = 1 + (βγ)_0^2
+tilde_m:  1 / (βγ)_0  # mc^2 / p0·c
+k2_num:   g / Bρ0 = g / (p0 / q)
+          where g and Bρ0 respectively denote the quadrupole gradient
+          and (signed) reference magnetic rigidity.
+L: element length
 """
-@inline function mkm_quadrupole!(i, v, work, beta_gamma_0, k2_num, L)
+@inline function mkm_quadrupole!(i, v, work, beta_0, gamsqr_0, tilde_m, k2_num, L)
   @assert size(work, 2) >= 5 && size(work, 1) == N_particle "Size of work matrix must be at least ($N_particle, 5) for mkm_quadrupole!()."
   @inbounds begin @FastGTPSA! begin
     quadrupole_matrix!(i, v, work, k2_num, L / 2)
-    quadrupole_kick!(i, v, work, beta_gamma_0, L)
+    quadrupole_kick!(  i, v, work, beta_0, gamsqr_0, tilde_m, L)
     quadrupole_matrix!(i, v, work, k2_num, L / 2)
   end end
   return v
@@ -98,7 +109,14 @@ end # function mkm_quadrupole!()
 """
 quadrupole_matrix!()
 
-track "matrix part" of a quadrupole
+Track "matrix part" of quadrupole.
+
+Arguments
+—————————
+k2_num:  g / Bρ0 = g / (p0 / q)
+         where g and Bρ0 respectively denote the quadrupole gradient
+         and (signed) reference magnetic rigidity.
+s: element length
 """
 @inline function quadrupole_matrix!(i, v, work, k2_num, s)
   @assert size(work, 2) >= 5 && size(work, 1) == N_particle "Size of work matrix must be at least ($N_particle, 5) for quadrupole_matrix!()."
@@ -125,7 +143,7 @@ end # function quadrupole_matrix!()
 """
 quadrupole_kick!()
 
-track "remaining part" of quadrupole, a position kick
+Track "remaining part" of quadrupole —— a position kick.
 
 ### Implementation
 A common factor that appears in the expressions for `zf.x` and `zf.y`
@@ -133,13 +151,17 @@ originally included a factor with the generic form ``1 - \\sqrt{1 - A}``,
 which suffers a loss of precision when ``|A| \\ll 1``. To combat that
 problem, we rewrite it in the form ``A / (1 + \\sqrt{1-A})``---more
 complicated, yes, but far more accurate.
+
+Arguments
+—————————
+beta_0:   β_0 = (βγ)_0 / √(γ_0^2)
+gamsqr_0: γ_0^2 = 1 + (βγ)_0^2
+tilde_m:  1 / (βγ)_0  # mc^2 / p0·c
+s: element length
 """
-@inline function quadrupole_kick!(i, v, work, beta_gamma_0, s)
+@inline function quadrupole_kick!(i, v, work, beta_0, gamsqr_0, tilde_m, s)
   @assert size(work, 2) >= 3 && size(work, 1) == N_particle "Size of work matrix must be at least ($N_particle, 3) for quadrupole_kick!"
   @inbounds begin @FastGTPSA! begin
-  tilde_m  = 1 / beta_gamma_0  # mc^2 / p0·c
-  gamsqr_0 = 1 + beta_gamma_0^2
-  beta_0   = beta_gamma_0 / sqrt(gamsqr_0)
   work[i,1] = 1 + v[i,PZI]             # reduced momentum, P/P0 = 1 + δ
   work[i,2] = v[i,PXI]^2 + v[i,PYI]^2  # P⟂^2
   work[i,3] = sqrt(p^2 - ptr2)         # Ps
@@ -164,115 +186,114 @@ end # function quadrupole_kick!()
 dkd_multipole()
 
 This integrator uses Drift-Kick-Drift to track a beam through
-a straight, finite-length multipole magnet. This method is
+a straight, finite-length multipole magnet. The method is
 accurate through second order in the step size. The vectors
-bm and am contain the normal and skew multipole strengths, starting
-with the dipole component. (For example, b[3] denotes the normal
-sextupole strength in Tesla/m^2.) The parameter ns denotes the
+kn and ks contain the normal and skew multipole strengths,
+starting with the dipole component. (For example, b[3] denotes
+the normal sextupole strength in Tesla/m^2.) The argument ns
+denotes the number of slices.
 
 Arguments
----------
-beta_gamma_0: reference value of βγ
-kn: normal multipole strengths
-ks: skew multipole strengths
+—————————
+beta_0:   β_0 = (βγ)_0 / √(γ_0^2)
+gamsqr_0: γ_0^2 = 1 + (βγ)_0^2
+tilde_m:  1 / (βγ)_0  # mc^2 / p0·c
+mm: vector of m values for non-zero multipole coefficients
+kn: vector of normal multipole strengths scaled by Bρ0
+ks: vector of skew multipole strengths scaled by Bρ0
 L:  element length
 """
-@inline function dkd_multipole!(i, v, work, beta_gamma_0, b_rho, kn, ks, L)
-  @assert size(work, 2) >= 5 && size(work, 1) == N_particle "Size of work matrix must be at least ($N_particle, 5) for dkd_multipole!()."
+@inline function dkd_multipole!(i, v, work, beta_0, gamsqr_0, tilde_m, mm, kn, ks, L)
+  @assert size(work, 2) >= 3 && size(work, 1) == N_particle "Size of work matrix must be at least ($N_particle, 3) for dkd_multipole!()."
   @inbounds begin @FastGTPSA! begin
-  tilde_m  = 1 / beta_gamma_0  # mc^2 / p0·c
-  gamsqr_0 = 1 + beta_gamma_0^2
-  beta_0   = beta_gamma_0 / sqrt(gamsqr_0)
   # ds = L / ns
   # for i = 1:ns
-  exact_drift!(i, v, work, tilde_m, gamsqr_0, beta_0, L / 2)
+  exact_drift!(   i, v, work, beta_0, gamsqr_0, tilde_m, L / 2)
   multipole_kick!(i, v, work, mm, kn * L, ks * L)
-  exact_drift!(i, v, work, tilde_m, gamsqr_0, beta_0, L / 2)
+  exact_drift!(   i, v, work, beta_0, gamsqr_0, tilde_m, L / 2)
   # end
   end end
   return v
 end # function dkd_multipole!()
 
 
+"""
+This function tracks a beam of particles through a thin-lens
+multipole having integrated normal and skew strengths in the
+coefficient vectors knl and ksl respectively. The vector mm
+lists the order of the corresponding entries in knl and ksl.
+
+NB: This function IGNORES the m = 1 (dipole) components of
+both knl and ksl. Moreover, it should *not* include an m = 2
+(quadrupole) component unless this function is used for a
+thin-lens (zero-length) element.
+
+The algorithm used in this function takes advantage of the
+complex representation of the vector potential Az,
+  - Re{ Σ_m (bm + i am) (x + i y)^m / m },
+and uses a Horner-like scheme (see Shachinger and Talman
+[SSC-52]) to compute the transverse kicks induced by a pure
+multipole magnet.  This method supposedly has good numerical
+properties, though I've not seen a proof of that.
+
+Arguments
+—————————
+mm:  vector of m values for non-zero multipole coefficients
+knl: vector of normal integrated multipole strengths
+ksl: vector of skew integrated multipole strengths
+     NB: Here the j-th component of knl (ksl) denotes the
+       normal (skew) component of the multipole strength of
+       order mm[j] (after scaling by the reference Bρ).
+       For example, if mm[j] = 3, then knl[j] denotes the
+       normal integrated sextupole strength scaled by Bρo.
+"""
 @inline function multipole_kick!(i, v, work, mm, knl, ksl)
-  """
-  This function tracks a beam of particles through a thin-lens
-  multipole having integrated normal and skew strengths in the
-  coefficient vectors knl and ksl respectively.
-  NP: This function IGNORES the m = 1 (dipole) components.
-  """
-  @assert size(work, 2) >= 2 && size(work, 1) == N_particle "Size of work matrix must be at least ($N_particle, 2) for multipole_kick!()."
+  @assert size(work, 2) >= 3 && size(work, 1) == N_particle "Size of work matrix must be at least ($N_particle, 3) for multipole_kick!()."
   @inbounds begin @FastGTPSA! begin
-  [ work[i,1], work[i,2] ] = dpxy_multipole(mm, knl, ksl, v[i,XI], v[i,YI])
-  v[i,PXI] = v[i,PXI] + work[i,1]
-  v[i,PYI] = v[i,PYI] + work[i,2]
-  end end
+  jm = length(mm)
+  m = mm[jm]
+  if m == 1 return v end
+  work[i,1] = ar = knl[jm] * v[i,XI] - ksl[jm] * v[i,YI]
+  work[i,2] = ai = knl[jm] * v[i,YI] + ksl[jm] * v[i,XI]
+  jm -= 1
+  while m > 2
+    m -= 1
+    work[i,3] = work[i,1] * v[i,XI] - work[i,2] * v[i,YI]
+    work[i,2] = work[i,1] * v[i,YI] + work[i,2] * v[i,XI]
+    work[i,1] = work[i,3]
+    if m == mm[jm]
+      work[i,1] += knl[jm] * v[i,XI] - ksl[jm] * v[i,YI]
+      work[i,2] += knl[jm] * v[i,YI] + ksl[jm] * v[i,XI]
+      jm -= 1
+    end
+  end
+  v[i,PXI] -= work[i,1]
+  v[i,PYI] += work[i,2]
   return v
 end # function multipole_kick!()
 
 
-function dpxy_multipole(mm, knl, ksl, x, y)
-  """
-  This function uses a Horner-like scheme (see Shachinger and
-  Talman [SSC-52]) to compute the transverse kicks induced by
-  a pure multipole magnet. The algorithm takes advantage of
-  the complex representation of the vector potential Az:
-   - Re{ Σ_m (bm + i am) (x + i y)^m / m }.
-  This method supposedly has good numerical properties, though
-  I've not seen a proof of that.
-
-  NB: This function IGNORES the m = 1 (dipole) components of
-  both knl and ksl. Moreover, it should *not* include an m = 2
-  (quadrupole) component unless this function is used for a
-  thin-lens (zero-length) element.
-
-  Arguments
-  ---------
-  mm:  maximum order, also length of both knl and ksl having
-         non-zero coefficients
-  knl: vector of normal integrated multipole strengths
-  ksl: vector of skew integrated multipole strengths
-       NB: Here the m-th component of knl (ksl) denotes the
-         normal (skew) component of the m-th multipole after
-         scaling by the reference Bρ. For example, knl[2]
-         denotes the normal integrated quadrupole strength
-         scaled by Bρo.
-  x:   horizontal particle coordinate
-  y:   vertical particle coordinate
-  """
-  ar = knl[m] * x - ksl[m] * y
-  ai = knl[m] * y + ksl[m] * x
-  while m > 2,
-    m -= 1
-    t  = (knl[m] * x - ksl[m] * y) + (ar * x - ai * y)
-    ai = (knl[m] * y + ksl[m] * x) + (ar * y + ai * x)
-    ar = t
-  end
-  return [ -ar, ai ]
-end # function dpxy_multipole()
-
-
-function binom(m::Integer, x, y)
-  """
-  This function computes the real and imaginary parts of
-  (x + i y)^m. One can use these components to compute,
-  among other things, the multipole kick induced by two-
-  dimensional multipole magnets.
-  """
-  if m == 0
-    return [ 1.0, 0.0 ]
-  end
-  ar = x
-  ai = y
-  mm = m
-  while mm > 1
-    mm -= 1
-    t  = x * ar - y * ai
-    ai = y * ar + x * ai
-    ar = t
-  end
-  return [ ar, ai ]
-end # function binom()
+#function binom(m::Integer, x, y)
+#  """
+#  This function computes the real and imaginary parts of
+#  (x + i y)^m. One can use these components to compute,
+#  among other things, the multipole kick induced by two-
+#  dimensional multipole magnets.
+#  """
+#  if m == 0
+#    return [ 1.0, 0.0 ]
+#  end
+#  ar = x
+#  ai = y
+#  mm = m
+#  while mm > 1
+#    mm -= 1
+#    t  = x * ar - y * ai
+#    ai = y * ar + x * ai
+#    ar = t
+#  end
+#  return [ ar, ai ]
+#end # function binom()
 
 
 end
