793a6a6e69e28199e7371890b2c3e3e933e0ad56
ndwang <ndwang3@gmail.com>
2025-06-05 19:43:12 -0400

    Explicit RK4 integration kernel

--- src/BeamTracking.jl
+++ src/BeamTracking.jl
@@ -12,6 +12,7 @@ export Bunch, Species, ParticleView, ELECTRON, POSITRON, PROTON, ANTIPROTON, sin
 export LinearTracking, Linear
 export ExactTracking, Exact
 export FieldTracking, Field
+export RungeKuttaTracking, RungeKutta
 export track!
 
 include("utils.jl")
@@ -22,6 +23,7 @@ include("types.jl")
 include("modules/ExactTracking.jl") #; TRACKING_METHOD(::ExactTracking) = Exact
 include("modules/LinearTracking.jl") #; TRACKING_METHOD(::LinearTracking) = Linear
 include("modules/FieldTracking.jl") #; TRACKING_METHOD(::FieldTracking) = Field
+include("modules/RungeKuttaTracking.jl") #; TRACKING_METHOD(::RungeKuttaTracking) = RungeKutta
 
 # Empty tracking method to be imported+implemented by package extensions
 function track! end
