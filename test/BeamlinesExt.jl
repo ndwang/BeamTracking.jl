b6254b4028e449b12d93759720e934398608fe0a
mattsignorelli <mgs255@cornell.edu>
2025-08-04 15:55:21 -0700

    fix again?

--- test/BeamlinesExt.jl
+++ test/BeamlinesExt.jl
@@ -585,7 +585,7 @@
     v_init = copy(b0.coords.v)
     ele_quad = LineElement(L=1.0, Kn1=1e-8, tracking_method=MatrixKick())
     track!(b0, Beamline([ele_quad], R_ref=R_ref))
-    @test b0.state[1] == State.Lost
+    @test b0.coords.state[1] == State.Lost
     @test v_init == b0.coords.v
 
     # Particle lost in patch (momentum is too small):
@@ -593,7 +593,7 @@
     v_init = copy(b0.coords.v)
     ele_patch = LineElement(dt=1e-9, dx=2.0, dy=3.0, dz=4.0, dx_rot=-5.0, dy_rot=6.0, dz_rot=7.0, L=-1.9458360380198412, tracking_method=SplitIntegration())
     track!(b0, Beamline([ele_patch], R_ref=R_ref))
-    @test b0.state[1] == State.Lost
+    @test b0.coords.state[1] == State.Lost
     @test v_init == b0.coords.v
 
     # Errors:
