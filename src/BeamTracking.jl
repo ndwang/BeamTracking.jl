cd4abc98934da4bf01962c2b84fcb68979364d10
mattsignorelli <mgs255@cornell.edu>
2025-09-14 18:20:45 -0400

    fix names

--- src/BeamTracking.jl
+++ src/BeamTracking.jl
@@ -34,8 +34,8 @@ include("types.jl")
 include("time.jl")
 include("kernel.jl")
 
-include("kernels/alignment_kernal.jl")
-include("kernels/aperture_kernal.jl")
+include("kernels/alignment_kernel.jl")
+include("kernels/aperture_kernel.jl")
 include("kernels/coord_rotation.jl")
 include("modules/ExactTracking.jl") #; TRACKING_METHOD(::ExactTracking) = Exact
 include("modules/LinearTracking.jl") #; TRACKING_METHOD(::LinearTracking) = Linear
