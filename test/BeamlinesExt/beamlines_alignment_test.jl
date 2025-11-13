964ca8269d9bc539594f5f9e4021e3ae1bd7b186
David Sagan <dcs16@cornell.edu>
2025-11-13 11:44:56 -0500

    Added alignment kicker test.

--- test/BeamlinesExt/beamlines_alignment_test.jl
+++ test/BeamlinesExt/beamlines_alignment_test.jl
@@ -3,7 +3,7 @@ include("../lattices/alignment_lat.jl")
 v1 = [0.1  0.2  0.3  0.4  0.5  0.6]
 drift_out = [0.3603778219616477 0.20000000000000004 0.8207556439232953 0.4 0.5565025848905005 0.6]
 bend1_out = [0.46408077686019533 0.25826924569290155 0.9250980107869189 0.46372657485893526 0.39699731659260634 0.6]
-v3 = [0.0 0.0 0.0 0.0 0.0 0.0]
+kicker1_out = [0.35583409051960385 0.19007681187771291 0.8203003409845823 0.399362770761857 0.5571770838946251 0.6]
 
 @testset "BeamlinesAlignment" begin
   b1 = Bunch(deepcopy(v1), species=Species("electron"))
@@ -21,4 +21,8 @@ v3 = [0.0 0.0 0.0 0.0 0.0 0.0]
   b4 = Bunch(deepcopy(v1), species=Species("electron"))
   track!(b4, bline_bend2)
   @test b4.coords.v ≈ drift_out
+
+  b5 = Bunch(deepcopy(v1), species=Species("electron"))
+  track!(b5, bline_kicker1)
+  @test b5.coords.v ≈ kicker1_out
 end
