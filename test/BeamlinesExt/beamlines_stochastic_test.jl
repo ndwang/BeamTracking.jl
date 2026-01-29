0e03d3a90840d65219f967f0ff993f7bfe46c09c
mattsignorelli <mgs255@cornell.edu>
2026-01-29 10:18:31 -0500

    stochastic radiation only acts on primitive types

--- test/BeamlinesExt/beamlines_stochastic_test.jl
+++ test/BeamlinesExt/beamlines_stochastic_test.jl
@@ -121,5 +121,19 @@ using Random
   radiation_damping_on = true, radiation_fluctuations_on = true)
   b0 = Bunch(rand(10,6), species = line.species_ref, p_over_q_ref = line.p_over_q_ref)
   track!(b0, line)
+
+  # Track TPSA make sure no errors, and equivalent with fluctuations on vs. off
+  bend.tracking_method = Yoshida(order = 2, num_steps = 1, 
+  radiation_damping_on = true, radiation_fluctuations_on = true)
+  b0 = Bunch(vars(D1), species = line.species_ref, p_over_q_ref = line.p_over_q_ref)
+  track!(b0, line)
+
+  # Track TPSA make sure no errors
+  bend.tracking_method = Yoshida(order = 2, num_steps = 1, 
+  radiation_damping_on = true, radiation_fluctuations_on = false)
+  b02 = Bunch(vars(D1), species = line.species_ref, p_over_q_ref = line.p_over_q_ref)
+  track!(b02, line)
+
+  @test norm(normTPS.(b0.coords.v - b02.coords.v)) ≈ 0
 end
  
