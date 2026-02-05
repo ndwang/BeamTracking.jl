00b75f0d1313ce21ad53f88254cb824471c64d8e
mattsignorelli <mgs255@cornell.edu>
2026-02-05 16:31:23 -0500

    make first test

--- test/runtests.jl
+++ test/runtests.jl
@@ -195,10 +195,10 @@ function quaternion_coeffs_approx_equal(q_expected, q_calculated, ϵ)
   return all_ok
 end
 
+include("batch_test.jl")
 include("BeamlinesExt_test.jl")
 include("alignment_tracking_test.jl")
 include("aperture_tracking_test.jl")
 include("ExactTracking_test.jl")
 include("IntegrationTracking_test.jl")
 include("time_test.jl")
-include("batch_test.jl")
